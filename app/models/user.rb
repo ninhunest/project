82a2c167d731abac2388cbbd157169ebc265e245
nguyen phan quang ninh <nguyenninhquang@gmail.com>
2017-11-15 15:57:57 +0700

    fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_many :microposts, dependent: :destroy
   has_many :comments
   has_many :active_relationships, class_name: "Relationship",
@@ -14,6 +15,25 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?token
+  end
+
   def feed
     microposts
   end
@@ -32,5 +52,18 @@ class User < ApplicationRecord
 
   def following? other_user
     following.include?(other_user)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
